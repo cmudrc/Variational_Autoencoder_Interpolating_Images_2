c606c32edd6e4a715cb810324762b5d0ea95c05d
mebaldwi <mebaldwi@andrew.cmu.edu>
2022-01-20 20:24:39 -0500

    Interpolation was removed from within the VAE code. Code was cleaned up further.

--- Variational_Autoencoder_Boxes.py
+++ Variational_Autoencoder_Boxes.py
@@ -95,8 +95,6 @@ print("Latent Encoding" + str(latent_encoding.shape))
 print("Encoder Input Data" + str(input_data.shape))
 encoder_model_boxes = tensorflow.keras.Model(input_data, latent_encoding)
 encoder_model_boxes.summary()
-
-
 ########################################################################################################################
 # The framework for the decoder is established
 decoder_input = tensorflow.keras.layers.Input(shape=latent_dimensionality)
@@ -122,7 +120,6 @@ encoded = encoder_model_boxes(input_data)
 decoded = decoder_model(encoded)
 autoencoder = tensorflow.keras.models.Model(input_data, decoded)
 autoencoder.summary()
-
 ########################################################################################################################
 # Autoencoder is trained using the training data
 
@@ -147,9 +144,7 @@ def get_loss(distribution_mean, distribution_variance):
 
 
 autoencoder.compile(loss=get_loss(distribution_mean, distribution_variance), optimizer='adam')
-autoencoder.fit(train_data, train_data, epochs=200, batch_size=32, validation_data=(test_data, test_data))
-
-
+autoencoder.fit(train_data, train_data, epochs=150, batch_size=32, validation_data=(test_data, test_data))
 ########################################################################################################################
 # Saving the Encoder Model
 # Saving model architecture to JSON file and Weights Separately
@@ -161,134 +156,9 @@ with open('model.json', 'w') as json_file:
 
 # Saving weights of model
 encoder_model_boxes.save_weights('model_tf', save_format='tf')  # tf format
-
 ########################################################################################################################
 # Model to Generate New Images
 decoder_model.save('decoder_model_boxes')
-########################################################################################################################
-# Used to print out an interpolation between two values
-test_data = np.reshape(testX, (len(testX), image_size, image_size, 1))
-
-
-'''  # USE WHEN TEST DATA SET IS LARGER!!!!!!!!!!!!!!!
-
-# Selecting a particular set of boxes for interpolation
-box_density_1 = 0.2
-box_density_2 = 1
-
-additional_pixels_1 = 1
-additional_pixels_2 = 0
-
-box_shape_1 = "Basic_Box"  # Select from "Basic_Box", "Diagonal_Box_Split", and "Horizontal_Box_Split"
-box_shape_2 = "Basic_Box"
-
-# Creates a sequence of input values for the desired label of number_1 and number_2
-# number_1 = [i for i in range(len(testX)) if (box_density_test[i] == box_density_1 and additional_pixels_test[i] == additional_pixels_1 and box_shape_test[i] == box_shape_1)]
-#for i in range(len(testX)):
- #   if box_density_test[i] == box_density_1 and additional_pixels_test[i] == additional_pixels_1 and box_shape_test[i] == box_shape_1:
-      #  number_1 = i
-'''
-# Randomly selects two points for interpolation
-number_1 = [i for i in range(len(testX))]
-number_1 = random.choice(number_1)  # choose a random index
-
-number_2 = [i for i in range(len(testX)) if i != number_1]
-number_2 = random.choice(number_2)
-
-
-# resize the array to match the prediction size requirement
-number_1_expand = np.expand_dims(number_1, axis=0)
-number_2_expand = np.expand_dims(number_2, axis=0)
-
-# Determine the latent point that will represent our desired number
-xy1 = encoder_model_boxes.predict(test_data[number_1_expand])
-xy2 = encoder_model_boxes.predict(test_data[number_2_expand])
-
-xy1 = xy1[0]
-xy2 = xy2[0]
-latent_dimensionality = len(xy1)
-print(xy1)
-print(xy2)
-########################################################################################################################
-# Establish the Framework of the Interpolation
-number_internal = 13  # the number of interpolations that the model will find between two points
-num_interp = number_internal + 2  # the number of images to be pictured
-figure = np.zeros((28, 28 * num_interp))  # The matrix that will hold the pixel values of the images
-#xy1 = [0, -3]  # Latent Point 1
-#xy2 = [0, 3]  # Latent Point 2
-x_values = np.linspace(xy1[0], xy2[0], num_interp)  # Interpolates the values of x between the two latent points
-
-print("Linspace X")
-#print(x_values)
-y_values = np.linspace(xy1[1], xy2[1], num_interp) # Interpolates the values of y between the two latent points
-print("Linspace Y")
-#print(y_values)
-
-########################################################################################################################
-# Interpolate the Images and Print out to User
-print('Latent Points')
-for iy, y in enumerate(y_values):  # enumerate creates a list of tuples [(0,-3), ... (29, 3)]
-    # iy will print out the column numbers
-    latent_point = np.array([[x_values[iy], y]])  # this creates the points that will establish the framework of the matrix
-    # for the images to populate around, in this case makes:
-    # [ [0,-3]  ... [0,3] ]
-    print(latent_point)
-    generated_image = decoder_model.predict(latent_point)[0]  # generates an interpolated image based on the latent point
-    figure[0: 28,  iy * 28:(iy + 1) * 28, ] = generated_image[:, :, -1]  # Inserts the Pixel Value for Each Image
-
-print("First Interpolation Point:\n" + str(box_shape_test[number_1]) + "\nPixel Density: " + str(
-            box_density_test[number_1]) + "\nAdditional Pixels: " + str(additional_pixels_test[number_1]))
-print("Box Matrix")
-print(box_matrix_test[number_1])
-print("Additional Pixels Test")
-print(additional_pixels_test)
-plt.imshow(testX[number_1], cmap='gray')
-
-plot_rows = 1
-plot_columns = 3
-plot_height = 1
-plot_width = plot_height*num_interp
-plt.figure(figsize=(plot_width, plot_height))
-plt.subplot(plot_rows, plot_columns, 1), plt.imshow(testX[number_1], cmap='gray')
-plt.title("First Interpolation Point:\n" + str(box_shape_test[number_1]) + "\nPixel Density: " + str(
-            box_density_test[number_1]) + "\nAdditional Pixels: " + str(additional_pixels_test[number_1]))
-plt.subplot(plot_rows, plot_columns, 2), plt.imshow(figure, cmap='gray')
-plt.title("Interpolation from First to Second Interpolation Point")
-plt.subplot(plot_rows, plot_columns, 3), plt.imshow(testX[number_2], cmap='gray')
-plt.title("Second Interpolation Point:\n" + str(box_shape_test[number_2]) + "\nPixel Density: " + str(
-            box_density_test[number_2]) + "\nAdditional Pixels: " + str(additional_pixels_test[number_2]))
-''' # USE AS LAST RESORT INTERPOLATION
-# Establish the Framework of the Interpolation between two random latent points
-number_internal = 13  # the number of interpolations that the model will find between two points
-num_interp = number_internal + 2  # the number of images to be pictured
-figure = np.zeros((28, 28 * num_interp))  # The matrix that will hold the pixel values of the images
-xy1 = [0, -3]  # Latent Point 1
-xy2 = [0, 3]  # Latent Point 2
-x_values = np.linspace(xy1[0], xy2[0], num_interp)  # Interpolates the values of x between the two latent points
-
-print("Linspace X")
-print(x_values)
-y_values = np.linspace(xy1[1], xy2[1], num_interp) # Interpolates the values of y between the two latent points
-print("Linspace Y")
-print(y_values)
-
-########################################################################################################################
-# Interpolate the Images and Print out to User
-print('Latent Points')
-for iy, y in enumerate(y_values):  # enumerate creates a list of tuples [(0,-3), ... (29, 3)]
-    # iy will print out the column numbers
-    latent_point = np.array([[x_values[iy], y]])  # this creates the points that will establish the framework of the matrix
-    # for the images to populate around, in this case makes:
-    # [ [0,-3]  ... [0,3] ]
-    print(latent_point)
-    generated_image = decoder_model.predict(latent_point)[0]  # generates an interpolated image based on the latent point
-    figure[0: 28,  iy * 28:(iy + 1) * 28, ] = generated_image[:, :, -1]  # Inserts the Pixel Value for Each Image
-
-plt.figure()
-plt.imshow(figure,cmap='gray')
-plt.show()
-'''
-
 ########################################################################################################################
 # Latent Feature Cluster for Test Data
 x = []
