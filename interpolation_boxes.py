cca7fdb0c79ef2651fbd771a3e97d5f302833e72
mebaldwi <mebaldwi@andrew.cmu.edu>
2022-01-20 19:25:04 -0500

    Finished the system to interpolate between two latent points with various dimensionality, and simplified the code to its core components. Comments were added to clarify the code further.

--- interpolation_boxes.py
+++ interpolation_boxes.py
@@ -3,14 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow
 import warnings
-from box_data_set import make_boxes
 from tensorflow.python.framework.ops import disable_eager_execution
-from sklearn.model_selection import train_test_split
 warnings.filterwarnings('ignore')
 disable_eager_execution()
-
+########################################################################################################################
+# Import the Models created by the VAE
 # import the decoder model
-decoder_model = tensorflow.keras.models.load_model('decoder_model_boxes')
+decoder_model_boxes = tensorflow.keras.models.load_model('decoder_model_boxes')
 
 # import the encoder model architecture
 json_file_loaded = open('model.json', 'r')
@@ -20,23 +19,20 @@ loaded_model_json = json_file_loaded.read()
 encoder_model_boxes = tensorflow.keras.models.model_from_json(loaded_model_json)
 
 # load weights into newly loaded_model
-# encoder_model.load_weights('model_h5')
 encoder_model_boxes.load_weights('model_tf')
 ########################################################################################################################
-# Import the Test Data
-image_size = 28
-load_data = np.load('train_test_split_data.npz')
-# box_data = make_boxes(image_size)
-# box_data_train, box_data_test = train_test_split(box_data, test_size=0.15)  # Uses a similar percentage as MNIST Data
+# Import the Test and Training Data used by the VAE
+load_data = np.load('train_test_split_data.npz')  # Data saved by the VAE
 
-# box_matrix_train, box_density_train, additional_pixels_train, box_shape_train = list(zip(*box_data_train))[0], list(zip(*box_data_train))[1], list(zip(*box_data_train))[2], list(zip(*box_data_train))[3]
+# Convert Data to Tuples and Assign to respective variables
 box_matrix_train, box_density_train, additional_pixels_train, box_shape_train = tuple(load_data['box_matrix_train']), tuple(load_data['box_density_train']), tuple(load_data['additional_pixels_train']), tuple(load_data['box_shape_train'])
-# box_matrix_test, box_density_test, additional_pixels_test, box_shape_test = list(zip(*box_data_test))[0], list(zip(*box_data_test))[1], list(zip(*box_data_test))[2], list(zip(*box_data_test))[3]
 box_matrix_test, box_density_test, additional_pixels_test, box_shape_test = tuple(load_data['box_matrix_test']), tuple(load_data['box_density_test']), tuple(load_data['additional_pixels_test']), tuple(load_data['box_shape_test'])
-testX = box_matrix_test
+testX = box_matrix_test  # Shows the relationship to the MNIST Dataset vs the Shape Dataset
+image_size = np.shape(testX)[-1]  # Determines the size of the images
 test_data = np.reshape(testX, (len(testX), image_size, image_size, 1))
 ########################################################################################################################
-"""
+# Select Latent Points to Interpolate Between
+""" # USE ONCE TEST DATA IS LARGE ENOUGH
 # Selecting a particular set of boxes for interpolation
 box_density_1 = 0.2
 box_density_2 = 1
@@ -50,63 +46,14 @@ box_shape_2 = "Basic_Box"
 # Creates a sequence of input values for the desired label of number_1 and number_2
 number_1 = [i for i in range(len(testX)) if box_density_test[i] == box_density_1 and additional_pixels_test[i] == additional_pixels_1 and box_shape_test[i] == box_shape_1]
 number_2 = [i for i in range(len(testX)) if box_density_test[i] == box_density_2 and additional_pixels_test[i] == additional_pixels_2 and box_shape_test[i] == box_shape_2]
-
-# choose a random index
-# number_1 = random.choice(indices_1)
-# number_2 = random.choice(indices_2)
-
-# resize the array to match the prediction size requirement
-number_1 = np.expand_dims(number_1, axis=0)
-number_2 = np.expand_dims(number_2, axis=0)
-print(number_1)
-# Determine the latent point that will represent our desired number
-xy1 = encoder_model.predict(test_data[number_1])
-xy2 = encoder_model.predict(test_data[number_2])
-
-xy1 = xy1[0]
-xy2 = xy2[0]
-latent_dimensionality = len(xy1)
-print(xy1)
-print(xy2)
-########################################################################################################################
-# Establish the Framework of the Interpolation
-number_internal = 13  # the number of interpolations that the model will find between two points
-num_interp = number_internal + 2  # the number of images to be pictured
-figure = np.zeros((28, 28 * num_interp))  # The matrix that will hold the pixel values of the images
-#xy1 = [0, -3]  # Latent Point 1
-#xy2 = [0, 3]  # Latent Point 2
-x_values = np.linspace(xy1[0], xy2[0], num_interp)  # Interpolates the values of x between the two latent points
-
-print("Linspace X")
-#print(x_values)
-y_values = np.linspace(xy1[1], xy2[1], num_interp) # Interpolates the values of y between the two latent points
-print("Linspace Y")
-#print(y_values)
-
-########################################################################################################################
-# Interpolate the Images and Print out to User
-print('Latent Points')
-for iy, y in enumerate(y_values):  # enumerate creates a list of tuples [(0,-3), ... (29, 3)]
-    # iy will print out the column numbers
-    latent_point = np.array([[x_values[iy], y]])  # this creates the points that will establish the framework of the matrix
-    # for the images to populate around, in this case makes:
-    # [ [0,-3]  ... [0,3] ]
-    print(latent_point)
-    generated_image = decoder_model.predict(latent_point)[0]  # generates an interpolated image based on the latent point
-    figure[0: 28,  iy * 28:(iy + 1) * 28, ] = generated_image[:, :, -1]  # Inserts the Pixel Value for Each Image
-
-plt.figure()
-plt.imshow(figure,cmap='gray')
-plt.show()
 """
-#def interpolation_random(testX):
+
 # Randomly selects two points for interpolation
-number_1 = [i for i in range(len(testX))]
-number_1 = random.choice(number_1)  # choose a random index
+number_1 = [i for i in range(len(testX))]  # creates a list of possible indices
+number_1 = random.choice(number_1)  # chooses a random index
 
 number_2 = [i for i in range(len(testX)) if i != number_1]
-number_2 = random.choice(number_2)
-
+number_2 = random.choice(number_2)  # chooses a random index
 
 # resize the array to match the prediction size requirement
 number_1_expand = np.expand_dims(number_1, axis=0)
@@ -116,40 +63,26 @@ number_2_expand = np.expand_dims(number_2, axis=0)
 latent_point_1 = encoder_model_boxes.predict(test_data[number_1_expand])
 latent_point_2 = encoder_model_boxes.predict(test_data[number_2_expand])
 
+# Index the latent point as a point rather than an array
 latent_point_1 = latent_point_1[0]
 latent_point_2 = latent_point_2[0]
-latent_dimensionality = len(latent_point_1)
+latent_dimensionality = len(latent_point_1)  # define the dimensionality of the latent space
 ########################################################################################################################
-# Establish the Framework of the Interpolation
+# Establish the Framework a LINEAR Interpolation
 number_internal = 13  # the number of interpolations that the model will find between two points
 num_interp = number_internal + 2  # the number of images to be pictured
 figure = np.zeros((image_size, image_size * num_interp))  # The matrix that will hold the pixel values of the images
+latent_matrix = []
 
-
-x_values = np.linspace(latent_point_1[0], latent_point_2[0], num_interp)  # Interpolates the values of x between the two latent points
-
-print("Linspace X")
-#print(x_values)
-y_values = np.linspace(latent_point_1[1], latent_point_2[1], num_interp) # Interpolates the values of y between the two latent points
-print("Linspace Y")
-#print(y_values)
-
-matrix = []
 for column in range(latent_dimensionality):
     new_column = np.linspace(latent_point_1[column], latent_point_2[column], num_interp)
-    matrix.append(new_column)
-matrix = np.array(matrix).T  # Transposes the matrix so that each row can be easily indexed
+    latent_matrix.append(new_column)
+latent_matrix = np.array(latent_matrix).T  # Transposes the matrix so that each row can be easily indexed
 ########################################################################################################################
 # Interpolate the Images and Print out to User
-print('Latent Points')
-for iy, y in enumerate(y_values):  # enumerate creates a list of tuples [(0,-3), ... (29, 3)]
-    # iy will print out the column numbers
-    latent_point = np.array([[x_values[iy], y]])  # this creates the points that will establish the framework of the matrix
-    # for the images to populate around, in this case makes:
-    # [ [0,-3]  ... [0,3] ]
-    print(latent_point)
-    generated_image = decoder_model.predict(latent_point)[0]  # generates an interpolated image based on the latent point
-    figure[0: 28,  iy * 28:(iy + 1) * 28, ] = generated_image[:, :, -1]  # Inserts the Pixel Value for Each Image
+for latent_point in range(num_interp):  # cycles the latent points through the decoder model to create images
+    generated_image = decoder_model_boxes.predict(np.array([latent_matrix[latent_point]]))[0]  # generates an interpolated image based on the latent point
+    figure[0: image_size,  latent_point * image_size:(latent_point + 1) * image_size, ] = generated_image[:, :, -1]  # Inserts the Pixel Value for Each Image
 
 plot_rows = 1
 plot_columns = 3
