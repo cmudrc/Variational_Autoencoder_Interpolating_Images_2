7bd619b4ddde8cbe4241dd485c73a5ffe4006de4
mebaldwi <mebaldwi@andrew.cmu.edu>
2022-03-06 15:17:07 -0500

    cleaned

--- box_data_set.py
+++ box_data_set.py
@@ -3,13 +3,12 @@ from basic_box_shapes import basic_box, diagonal_box_split, horizontal_vertical_
 from complex_box_shapes import back_slash_box, forward_slash_box, back_slash_plus_box, forward_slash_plus_box, hot_dog_box, hamburger_box, x_hamburger_box, x_hot_dog_box, x_plus_box, center_box
 import matplotlib.pyplot as plt  # Use to verify various box shapes and densities
 # The data is made such that it can be unpacked from the following tuple:
-# (box_matrix, box_density, additional_pixels, box_shape)
+# (box_matrix, box_density, additional_pixels, box_shape, "ground_truth")
+# where "ground_truth" will be used to discern between various data augmentation techniques
 
 box_functions = [basic_box, diagonal_box_split, horizontal_vertical_box_split, back_slash_box, forward_slash_box,
                  back_slash_plus_box, forward_slash_plus_box, hot_dog_box, hamburger_box, x_hamburger_box,
                  x_hot_dog_box, x_plus_box]  # center_box was removed for the purpose of our use case
-
-# box_functions = [basic_box, diagonal_box_split, horizontal_vertical_box_split]
 
 
 ########################################################################################################################
